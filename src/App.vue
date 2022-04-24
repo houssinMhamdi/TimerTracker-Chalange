47b2ba734ba3875cadc799fe9aafcb0f9f06e794
Your Name <you@example.com>
2022-04-24 19:36:02 +0200

    Timer Tracker Chalange

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="">
+    <TimerTracker
+      :timer="formattedTime"
+      :state="timerState"
+      :laps="laps"
+      :tim="tim"
+      @start="start"
+      @lap="lap"
+      @stop="stop"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TimerTracker from "./components/TimerTracker";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { TimerTracker },
+  data() {
+    return {
+      timerState: "stopped",
+      currentTimer: 0,
+      formattedTime: "00:00:00",
+      ticker: undefined,
+      laps: [],
+      tim: "",
+      latestLap: "",
+    };
+  },
+
+  methods: {
+    start() {
+      if (this.timerState !== "running") {
+        this.tick();
+        this.timerState = "running";
+      }
+        this.tim = this.getTiem();
+      
+    },
+    lap() {
+      this.laps.push({
+        seconds: this.currentTimer,
+        formattedTime: this.formatTime(this.currentTimer),
+      });
+      this.latestLap = this.formatTime(this.currentTimer);
+      this.currentTimer = 0;
+    },
+
+    stop() {
+      this.laps.push({
+        seconds: this.currentTimer,
+        formattedTime: this.formatTime(this.currentTimer),
+      });
+      this.latestLap = this.formatTime(this.currentTimer);
+      this.currentTimer = 0;
+
+      window.clearInterval(this.ticker);
+      this.currentTimer = 0;
+      this.formattedTime = "00:00:00";
+      this.timerState = "stopped";
+    },
+    tick() {
+      this.ticker = setInterval(() => {
+        this.currentTimer++;
+        this.formattedTime = this.formatTime(this.currentTimer);
+      }, 250);
+    },
+    formatTime(seconds) {
+      let measuredTime = new Date(null);
+      measuredTime.setSeconds(seconds);
+      let MHSTime = measuredTime.toISOString().substr(11, 8);
+      return MHSTime;
+    },
+    getTiem() {
+      let today = new Date();
+      let time =
+        today.getHours() + ":" + today.getMinutes();
+      return time;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/TimerTracker.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class="container">
+    <div class="header">
+      <div class="today">
+        <span>Today, {{ getdate().day }} </span>
+        <span> {{ getdate().name }} </span>
+      </div>
+
+      <div class="btns-interactions">
+        <div class="btn-stop" @click="$emit('stop')">
+          <span>Stop</span>
+          <div class="rectangle"></div>
+        </div>
+
+        <div class="btn" @click="$emit('start')">
+          <span>Start New</span>
+          <div class="img"><img src="../assets/timer.svg" alt="timer" /></div>
+        </div>
+        <h1>{{ timer }}</h1>
+      </div>
+    </div>
+
+    <div v-for="(lap, index) in laps" :key="index" class="laps">
+      <span>Period {{ index + 1 }}:</span>
+      <span class="curenttime">
+        <div class="started">started</div>
+        <div class="br">{{ tim }}</div>
+        {{ lap.formattedTime }}
+      </span>
+    </div>
+
+    <div class="total">
+      <span> Total</span>
+      <span> 2:55:00 </span>
+    </div>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TimerTracker",
+  props: ["timer", "state", "laps", "tim"],
+
+  methods: {
+    getdate() {
+      const month = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      const d = new Date();
+      let name = month[d.getMonth()];
+      let day = d.getDate();
+      return { name, day };
+    },
+  },
+  //   total() {
+  //     const sum = this.laps.reduce((a, b) => a + b, 0);
+  //     console.log(sum);
+  //     return sum;
+  //   },
+};
+</script>
+
+<style>
+.btn {
+  width: 200px;
+  height: 38px;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 38px;
+  color: #60b669;
+  border-radius: 5px;
+  border: 1px solid #60b669;
+  margin-right: 5px;
+
+  position: relative;
+}
+.btn span {
+  font-weight: 600;
+}
+.btn-stop {
+  width: 120px;
+  height: 38px;
+  line-height: 38px;
+  color: #cc5252;
+  border-radius: 5px;
+  border: 1px solid #cc5252;
+  margin-right: 8px;
+  padding-left: 10px;
+  position: relative;
+  cursor: pointer;
+}
+.btn-stop span {
+  font-weight: 600;
+}
+.rectangle {
+  position: absolute;
+  top: 12px;
+  left: 10px;
+  width: 13px;
+  height: 13px;
+
+  background: #cc5252;
+}
+.header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border: none;
+  width: 80%;
+  padding-bottom: 50px;
+  margin: auto;
+  border-bottom: 1px solid #60b669;
+}
+.btns-interactions {
+  width: 350px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.today span {
+  font-weight: 900;
+  font-size: 30px;
+  color: #415c73;
+}
+.header h1 {
+  font-size: 25px;
+  color: #60b669;
+  margin-left: 15px;
+}
+.img {
+  width: 13px;
+  height: 15px;
+  color: #60b669;
+  position: absolute;
+  top: 2px;
+  left: 10px;
+}
+.laps {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 20px 0;
+  border: none;
+  border-bottom: 1px solid #60b669;
+  width: 80%;
+  margin: auto;
+}
+.laps span {
+  font-size: 16px;
+  color: #415c73;
+  font-weight: 900;
+}
+.total {
+  width: 84.5%;
+  margin: auto;
+  display: flex;
+  justify-content: flex-end;
+  margin-left: -150px;
+  font-size: 16px;
+  color: #6d7783;
+  font-weight: 900;
+  padding: 50px 0;
+}
+.total span {
+  padding: 15px 0;
+  margin-left: 10px;
+}
+.total span:last-child {
+  color: #60b669;
+}
+.line {
+  width: 80%;
+  margin: auto;
+  height: 1px;
+  background: #e8e8e8;
+}
+.br {
+  color: black;
+  width: 54px;
+  height: 20px;
+  font-size: 14px;
+  color: #6b7581;
+}
+.curenttime {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 200px;
+}
+.started {
+  color: #6b7581;
+  font-size: 15px;
+}
+</style>
